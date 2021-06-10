c3d7cdf3ed20bb0502c43b78bd32088bc838be44
EC2 Default User <ec2-user@ip-172-31-37-159.ec2.internal>
2021-06-10 12:47:15 +0000

    [ADD]

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def new
+   
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'homes#top'
+
+  resources :users
+  resources :books
+
+  get 'about' => 'abouts#top'
 
 end
