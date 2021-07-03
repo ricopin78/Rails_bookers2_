80dbb5f9dad2c2bd2d8020dc2451a6e5bdb53de2
EC2 Default User <ec2-user@ip-172-31-37-159.ec2.internal>
2021-07-03 10:48:36 +0000

    フォロー機能モデル・テーブル・アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,22 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
   attachment :profile_image
 
